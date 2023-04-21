d5bbd8e06faadaa7246d8334481fc31c4f117b3f
JorgeLuisMartinez <Jorge389.jm@gmail.com>
2023-04-21 17:03:14 -0500

    clase 9 hoisting

---
+++ hoisting.js
@@ -0,0 +1,16 @@
+// ejemplos
+
+/* var miNombre;
+
+miNombre = "master"
+
+var miNombre = "master" */
+
+console.log(miNombre);
+
+hey();
+function hey(){
+    console.log("hola", miNombre);
+}
+
+var miNombre = "master";
