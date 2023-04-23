3b8cfe7b6aa9d3208a78a9c767f58c8f46208947
JorgeLuisMartinez <Jorge389.jm@gmail.com>
2023-04-22 19:58:34 -0500

    clase 11 y clase 12

---
+++ operadores.js
@@ -0,0 +1,51 @@
+// Operadores de operación
+//Operadores binarios
+5 + 2;
+
+50 - 10;
+
+10 * 20;
+
+20 / 2;
+
+//Concatenar strings
+"Clear" + "Master";
+
+//Operadores unitarios
+// negación, como negar que false sea false:
+!false
+
+//Operadores de asignación el =
+var a = 1;
+
+//Operadores de comparacion ==
+3 == "3"
+// el resultado seria true puesto que el compara el contenido mas no valida el tipo de dato
+
+//Operador de validacion compara de manera estricta incluso el tipo de dato ===
+3 === "3"
+// el resultado seria false, puesto que uno dato es de tipo num y el otro de tipo string
+
+// Otros operadores de comparacion < > <= >=
+5 < 3
+// daria falso pues 5 no es menor que 3
+
+5 > 3
+// true, mas de lo mismo
+
+5 <= 6
+
+5 >= 6
+
+// operador "tambien" o "y"
+a && b
+
+// operador "o"
+a || b
+
+//operadores aritmeticos
+var edad = 40;
+// para incrementar el valor de la variable ++ disminuir -- aumentar por 2 =+ 2
+edad++;
+edad--;
+edad+= 2;
---
+++ truthy-falsy.js
@@ -0,0 +1,17 @@
+// Valores falsos:
+Boolean();
+Boolean (0);
+Boolean (null);
+Boolean (NaN);
+Boolean(undefined);
+Boolean(false);
+Boolean("");
+
+// Valores verdaderos:
+Boolean (1);
+Boolean ([]);
+Boolean ({});
+Boolean(function(){});
+Boolean(true);
+
+
